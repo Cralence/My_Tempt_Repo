2e08e36cd0331c0833bb3279d4a3b25da2b3e949
Cralence <mahlerrrr76@gmail.com>
2023-12-20 09:12:22 -0500

    debug

--- tempt.py
+++ tempt.py
@@ -1,7 +1,7 @@
 import torch
 
 ckpt_1 = '../../final_version_2/My_Tempt_Repo/mm_transformer_logs/train_music_motion/checkpoints/epoch=000025.ckpt'
-ckpt_2 = '../../final_version_2/My_Tempt_Repo/mm_transformer_logs/2023-12-14T20-48-06_train_lm/checkpoints/last.ckpt'
+ckpt_2 = 'training_logs/2023-12-19T23-33-58_train_lm/checkpoints/last.ckpt'
 
 a = torch.load(ckpt_1, map_location='cpu')['state_dict']
 b = torch.load(ckpt_2, map_location='cpu')['state_dict']
